615e078577f4a03ed721f68bbbfbb1468a150b2f
Strochak <sstrochak@urban.org>
2020-07-02 12:21:26 -0400

    new color palette for continous maps

--- R/maps/jpw-points.R
+++ R/maps/jpw-points.R
@@ -18,7 +18,8 @@ ls <- tibble(long = -87.688014,
 
 ls_points <- st_intersection(chi, ls) %>% 
   mutate(group5 = sample(1:5, nrow(.), replace = TRUE)) %>% 
-  mutate(grad = scales::rescale(as.numeric(x_coord), to = c(0, 1)))
+  mutate(grad = scales::rescale(as.numeric(x_coord), to = c(0, 1))) %>% 
+  mutate(grady = scales::rescale(as.numeric(y_coord), to = c(0, 1)))
 
 
 nhoods5 <- c("#DC8665", "#138086", "#534666", "#CD7672", "#EEB462")
@@ -32,8 +33,22 @@ ggplot() +
 ggsave("maps/lincoln-square-buldings.png",
        height = 9, width = 9, dpi = 500)
 
+
+pal1 <- c("#d2fbd4","#a5dbc2","#7bbcb0","#559c9e","#3a7c89","#235d72","#123f5a")
+pal2 <- c("#008080","#70a494","#b4c8a8","#f6edbd","#edbb8a","#de8a5a","#ca562c")
+
 ggplot() +
   geom_sf(data = ls_points, mapping = aes(fill = grad),
           color = NA) +
+  scale_fill_gradientn(colors = pal2) +
   urbnthemes::theme_urbn_map() +
   guides(fill = FALSE)
+ggsave("maps/lincoln-square-x.png", height = 9, width = 9)
+
+ggplot() +
+  geom_sf(data = ls_points, mapping = aes(fill = grady),
+          color = NA) +
+  scale_fill_gradientn(colors = pal2) +
+  urbnthemes::theme_urbn_map() +
+  guides(fill = FALSE)
+ggsave("maps/lincoln-square-y.png", height = 9, width = 9)
