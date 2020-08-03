c3f886f062f37169d6e3d9254975bf823b73e783
Strochak <sstrochak@urban.org>
2020-08-03 09:17:20 -0400

    update colors on remi map

--- R/maps/remi.R
+++ R/maps/remi.R
@@ -15,12 +15,13 @@ remi <- sf %>%
                                lat = 37.802322,
                                size = 1)) %>% 
   get_random_groups(var_name = group5, 
-                    groups = 5)
+                    groups = 4)
 
 map_buildings_discrete(remi,
-                       color_scheme = c("#257E78", "#40B8AF", "#6C2D58", "#B2577A", "#FFEEBB")) 
+                       color_scheme =c("#478BA2", "#DE5B6D", "#F2A490", "#B9D4DB")) 
 
-
+c("#FF7B89", "#8A5082", "#6F5F90", "#758EB7", "#A5CAD2")
+c("#257E78", "#40B8AF", "#6C2D58", "#B2577A", "#FFEEBB")
 c("#525564", "#74828F", "#96C0CE", "#BEB9B5", "#C25B56", "#FEF6EB")
 
-save_map(map_name = "remi")
+save_map(map_name = "remi-2")
